2a0481ffe491fda2a6284e267b484c923bde96aa
Thiago Arthur <thlimabsgim@gmail.com>
2026-01-03 18:03:34 -0400

    feat: restructure App component to implement navigation stack and include Onboarding screen

--- App.tsx
+++ App.tsx
@@ -1,21 +1,39 @@
-import { useFonts } from 'expo-font';
-import { AuthScreen } from './src/screens/AuthScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useFonts } from "expo-font";
+
+import { OnboardingScreen } from "./src/screens/OnboardingScreen";
+import { AuthScreen } from "./src/screens/AuthScreen";
+
+export type RootStackParamList = {
+  Onboarding: undefined;
+  Auth: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   const [loaded] = useFonts({
     "PlusJakartaSans-Bold": require("./assets/fonts/PlusJakartaSans-Bold.ttf"),
     "PlusJakartaSans-Medium": require("./assets/fonts/PlusJakartaSans-Medium.ttf"),
     "PlusJakartaSans-Regular": require("./assets/fonts/PlusJakartaSans-Regular.ttf"),
-    "PlusJakartaSans-SemiBold": require("./assets/fonts/PlusJakartaSans-SemiBold.ttf")
-  })
-  
+    "PlusJakartaSans-SemiBold": require("./assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+  });
+
   if (!loaded) {
     return null;
   }
-  
+
   return (
-      <AuthScreen />
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+        <Stack.Screen name="Auth" component={AuthScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-
