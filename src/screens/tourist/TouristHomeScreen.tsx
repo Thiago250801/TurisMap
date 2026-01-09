5ee4d692498399d8cb219f14419779eea0fb03c7
Thiago Arthur <thlimabsgim@gmail.com>
2026-01-09 13:49:45 -0400

    feat: enhance TouristHomeScreen with SearchBar and layout adjustments

--- src/screens/tourist/TouristHomeScreen.tsx
+++ src/screens/tourist/TouristHomeScreen.tsx
@@ -1,32 +1,47 @@
 import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from "react-native";
 import { MapPin, Bell } from "lucide-react-native";
 import { colors, fontFamily, radius } from "../../theme";
+import { SearchBar } from "../../components/SearchBar";
+import { useState } from "react";
+import { PlaceCard } from "../../components/PlaceCard";
+import teatroAmazonas from "../../assets/Teatro_Amazonas.jpg"
 
 export const TouristHomeScreen = () => {
+
+  const [search, setSearch] = useState("");
+
   return (
     <View style={styles.container}>
       {/* HEADER */}
-      <View style={styles.header}>
-        <View style={styles.logo}>
-          <View style={styles.logoIcon}>
-            <MapPin size={18} color={colors.primaryForeground} />
+<View style={styles.header}>
+        <View style={styles.topRow}>
+          <View style={styles.logo}>
+            <View style={styles.logoIcon}>
+              <MapPin size={18} color={colors.primaryForeground} />
+            </View>
+            <Text style={styles.logoText}>
+              turis<Text style={styles.logoPrimary}>map</Text>
+            </Text>
           </View>
-          <Text style={styles.logoText}>
-            turis<Text style={styles.logoPrimary}>map</Text>
-          </Text>
+
+          <TouchableOpacity style={styles.notification}>
+            <Bell size={22} color={colors.foreground} />
+            <View style={styles.badge} />
+          </TouchableOpacity>
         </View>
 
-        <TouchableOpacity style={styles.notification}>
-          <Bell size={22} color={colors.foreground} />
-          <View style={styles.badge} />
-        </TouchableOpacity>
+        {/* SearchBar inserida aqui */}
+        <SearchBar 
+          value={search} 
+          onChange={setSearch} 
+          placeholder="Buscar lugares, experiências..." 
+        />
       </View>
 
       {/* CONTENT */}
       <ScrollView contentContainerStyle={styles.content}>
         <Text style={styles.sectionTitle}>Popular entre os usuários</Text>
-        <Text style={styles.placeholder}>Cards virão aqui 👇</Text>
-
+        <PlaceCard title="Teatro Amazonas" rating={4.5} image={teatroAmazonas} />
         <Text style={styles.sectionTitle}>Sugestões do Turismap</Text>
         <Text style={styles.placeholder}>Cards virão aqui 👇</Text>
       </ScrollView>
@@ -42,14 +57,22 @@ const styles = StyleSheet.create({
   },
 
   header: {
-    padding: 24,
+    paddingHorizontal: 24,
     paddingTop: 56,
-    flexDirection: "row",
+    paddingBottom: 16,
     justifyContent: "space-between",
-    alignItems: "center",
+    backgroundColor: colors.background,
     borderBottomWidth: 1,
     borderColor: colors.border,
   },
+
+topRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 16, 
+  },
+
 
   logo: {
     flexDirection: "row",
