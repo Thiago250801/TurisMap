0cddc0ea5da4dd6c007c5008cd1665f6b5be1feb
Thiago Arthur <thlimabsgim@gmail.com>
2026-01-21 14:20:23 -0400

    feat: define Place type and FormData structure for planning functionality

---
+++ src/model/plan.ts
@@ -0,0 +1,12 @@
+export type Place = {
+  id: string;
+  title: string;
+  image: string;
+};
+
+type FormData = {
+  name: string;
+  startDate: string;
+  endDate: string;
+  offline: boolean;
+};
